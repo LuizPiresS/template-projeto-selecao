968846f9294715fbf3b30ef227d2e091aae5941e
Luiz Pires <kw13code@gmail.com>
2020-07-13 10:40:16 -0300

    test: ensure not duplicated email

--- backend/src/core/user/interactors/create-user.interactor.ts
+++ backend/src/core/user/interactors/create-user.interactor.ts
@@ -3,17 +3,24 @@ import { Validator } from '../../common/validator'
 import { CreateUserRequest } from '../dto/create-user.request'
 import { CreateUserResponse } from '../dto/create-user.response'
 import { UserEmailInvalidError } from '../errors'
+import { IUserRepository } from '../user.repository'
+import { UserDuplicatedEmailError } from './../errors/user-duplicated-email.error'
 
 export class CreateUserInteractor {
   constructor (
     protected readonly validator: Validator,
-    protected readonly presenter: Presenter<CreateUserResponse>
+    protected readonly presenter: Presenter<CreateUserResponse>,
+    protected readonly userRepository: IUserRepository
   ) {}
 
   async execute (data: CreateUserRequest): Promise<void> {
     try {
       if (!this.validator.isEmail(data.email)) {
         throw new UserEmailInvalidError('Invalid e-mail')
+      }
+
+      if (this.userRepository.findByEmail(data.email)) {
+        throw new UserDuplicatedEmailError('E-mail duplicated')
       }
     } catch (error) {
       await this.presenter.throw(error)
