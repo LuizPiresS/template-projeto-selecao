3acbccf854af84eeba496a6b20c187a95baea1c0
Luiz Pires <kw13code@gmail.com>
2020-07-13 05:47:44 -0300

    chore: add eslint-helper

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -5,5 +5,21 @@ module.exports = {
   extends: 'standard-with-typescript',
   parserOptions: {
     project: './tsconfig.json'
+  },
+  plugins: ['eslint-plugin-import-helpers'],
+  rules: {
+    '@typescript-eslint/no-floating-promises': 'off',
+    'import-helpers/order-imports': [
+      'warn',
+      { // example configuration
+        newlinesBetween: 'always',
+        groups: [
+          'module',
+          '/^@shared/',
+          ['parent', 'sibling', 'index']
+        ],
+        alphabetize: { order: 'asc', ignoreCase: true }
+      }
+    ]
   }
 }
