8c7202bea17cc938c8d945a42f25bd9fe28bb35c
Luiz Pires <kw13code@gmail.com>
2020-07-14 13:35:14 -0300

    chore: add new rules configuration

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -9,6 +9,10 @@ module.exports = {
   plugins: ['eslint-plugin-import-helpers'],
   rules: {
     '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
     'import-helpers/order-imports': [
       'warn',
       { // example configuration
