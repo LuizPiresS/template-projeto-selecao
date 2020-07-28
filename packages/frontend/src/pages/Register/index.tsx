63de179474f4a63e612d4f128dfd07fcc000c664
Luiz Pires <kw13code@gmail.com>
2020-07-28 07:13:35 +0000

    feat: create register page

--- packages/frontend/src/pages/Register/index.tsx
+++ packages/frontend/src/pages/Register/index.tsx
@@ -1,8 +1,56 @@
 import React from 'react'
-import { Title } from './styles'
+import logoImg from '../../assets/logoapp.svg'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import axios from 'axios'
+
+import { Title, FormCSS } from './styles'
+type FormValues = {
+  firstName: string
+  lastName: string
+  githubUsername: string
+  email: string
+  password: string
+}
 
 const Register: React.FC = () => {
-  return <Title>Register</Title>
+  const { register, handleSubmit } = useForm<FormValues>()
+  const onSubmit: SubmitHandler<FormValues> = data => {
+    axios
+      .post('http://localhost:3003/users', data)
+      .then(resp => {
+        const { data } = resp
+        if (data) {
+          localStorage.setItem('app-token', data.token)
+        }
+      })
+      .catch(error => {
+        console.log(error.response.data.error.message)
+      })
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Register" />
+      <Title>Register</Title>
+      <FormCSS onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="Primeiro nome" name="firstName" ref={register} />
+        <input placeholder="Sobrenome" name="lastName" ref={register} />
+        <input
+          placeholder="Nome de usuário github"
+          name="githubUsername"
+          ref={register}
+        />
+        <input placeholder="E-mail" name="email" ref={register} />
+        <input
+          placeholder="Password"
+          type="password"
+          name="password"
+          ref={register}
+        />
+        <button type="submit">cadastrar</button>
+      </FormCSS>
+    </>
+  )
 }
 
 export default Register
--- packages/frontend/src/pages/Register/styles.ts
+++ packages/frontend/src/pages/Register/styles.ts
@@ -1,6 +1,47 @@
 import styled from 'styled-components'
+import { shade } from 'polished'
 
 export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+  margin-top: 80px;
+`
+export const FormCSS = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+
+  /* display: flex; */
+
+  input {
+    display: block;
+    margin-top: 10px;
+    height: 50px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px;
+    color: #a3a3a3;
+
+    &:placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    display: block;
+    margin-top: 10px;
+    width: 210px;
+    height: 50px;
+    background: #04d361;
+    border: 0;
+    border-radius: 5px;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
 `
